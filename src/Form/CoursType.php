44d9c4c4292f007ff955d26d4f14b0ca801befac
jbdwwm48 <jb.charbonnier.dwwm48@gmail.com>
2025-04-25 12:10:47 +0200

    migration du refactoring Quiz

--- src/Form/CoursType.php
+++ src/Form/CoursType.php
@@ -8,7 +8,6 @@ use App\Entity\User;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -20,18 +19,12 @@ class CoursType extends AbstractType
         $builder
             ->add('titre')
             ->add('contenu')
-            ->add('createdAt', null, [
-                'widget' => 'single_text',
-            ])
-            ->add('user', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => 'id',
-            ])
-            ->add('motCles', EntityType::class, [
-                'class' => MotCle::class,
-                'choice_label' => 'id',
-                'multiple' => true,
-            ])
+
+            // ->add('motCles', EntityType::class, [
+            //     'class' => MotCle::class,
+            //     'choice_label' => 'id',
+            //     'multiple' => true,
+            // ])
         ;
     }
 
