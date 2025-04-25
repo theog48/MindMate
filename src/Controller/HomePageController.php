af5ad47b29d74ba520859e9972cb0d8b4add07d2
nadima-mokhtari <nadima.mokhtari.dwwm48@gmail.com>
2025-04-25 09:36:06 +0200

    update commentaire note

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CommentaireRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,14 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomePageController extends AbstractController
 {
     #[Route('/home/page', name: 'app_home_page')]
-    public function index(): Response
+    public function index(CommentaireRepository $commentaireRepository): Response
     {
+        // Store the result in a variable
+        $commentaires = $commentaireRepository->findAll();
+
         return $this->render('home_page/index.html.twig', [
             'controller_name' => 'HomePageController',
+            'commentaires' => $commentaires,
         ]);
     }
 }
