6046df2d31668526b42b26867e5bdeea17ddf4c6
Syllac21 <syllac.online@gmail.com>
2025-04-29 14:49:32 +0200

    changement de role en direct

--- src/Controller/PaidPageController.php
+++ src/Controller/PaidPageController.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use App\Form\PaidType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,7 +14,7 @@ use Symfony\Component\Routing\Attribute\Route;
 final class PaidPageController extends AbstractController
 {
     #[Route('/paid/page', name: 'app_paid_page')]
-    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    public function index(Request $request, EntityManagerInterface $entityManager, Security $security): Response
     {
         $user = $this->getUser();
         $form = $this->createForm(PaidType::class);
@@ -28,10 +29,10 @@ final class PaidPageController extends AbstractController
                 }else{
                     $newDateFin = new \DateTime($user->getDateFinPremium()->format('Y-m-d'));
                 }
-                // var_dump($newDateFin);
-                // var_dump($nbMois);
+        
                 $newDateFin->modify("+$nbMois month");
                 $user->setDateFinPremium($newDateFin);
+                $security->login($user, 'form_login', 'main');
                 $entityManager->persist($user);
                 $entityManager->flush();
             }
