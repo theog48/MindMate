3e6f4872ae2f271a5725ef7d6dd27c77b2167dcf
Syllac21 <syllac.online@gmail.com>
2025-04-29 14:02:20 +0200

    ajout de la fonction pour payer un mois

--- src/Controller/PaidPageController.php
+++ src/Controller/PaidPageController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Form\PaidType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,12 +13,28 @@ use Symfony\Component\Routing\Attribute\Route;
 final class PaidPageController extends AbstractController
 {
     #[Route('/paid/page', name: 'app_paid_page')]
-    public function index(Request $request): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $user = $this->getUser();
         $form = $this->createForm(PaidType::class);
         $form->handleRequest($request);
         $formData = $form->getData();
-        if($form)
+        $nbMois = $formData['NbMois'] ?? null;
+        if($form->isSubmitted() && $form->isValid()){
+            if($user instanceof User){
+                if($user->getDateFinPremium() == null || $user->getDateFinPremium() < new \DateTime()){
+                    $newDateFin = new \DateTime();
+                }else{
+                    $newDateFin = new \DateTime($user->getDateFinPremium()->format('Y-m-d'));
+                }
+                // var_dump($newDateFin);
+                // var_dump($nbMois);
+                $newDateFin->modify("+$nbMois month");
+                $user->setDateFinPremium($newDateFin);
+                $entityManager->persist($user);
+                $entityManager->flush();
+            }
+        }
 
         return $this->render('paid_page/index.html.twig', [
             'form' => $form->createView(),
