32c4695f4f5821b437a588f02013c280203d75a1
Celia506 <celia.sansano.dwwm48@gmail.com>
2025-04-29 15:25:53 +0200

    update

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,10 +3,8 @@
 namespace App\Form;
 
 use App\Entity\User;
-use Doctrine\DBAL\Types\IntegerType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
